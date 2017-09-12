81d03acc1c1091cb1133bdd041cbcf1164e5620b
Lakhmes <kosseila.lakhmes@gmail.com>
2017-09-12 10:21:21 +0200

    commit projet initial

---
+++ monAppliMavenGit/src/main/java/com/kosseila/metier/monAppliMavenGit/App.java
@@ -0,0 +1,13 @@
+package com.kosseila.metier.monAppliMavenGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
